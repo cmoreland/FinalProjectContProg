b07c3ac2e203671fe925e57fd2491bf8717213ce
Brian <faughtbn@mail.uc.edu>
2024-11-28 20:15:03 -0500

    Add DbSet properties for Projects, Tasks, and Skills in AppDbContext

--- FinalProjectContProg/Data/AppDbContext.cs
+++ FinalProjectContProg/Data/AppDbContext.cs
@@ -2,11 +2,15 @@
 using FinalProjectContProg.Models;
 using System.Collections.Generic;
 
-namespace FinalProjectContProg.Data;
-
-public class AppDbContext : DbContext
+namespace FinalProjectContProg.Data
 {
-    public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
 
-    public DbSet<TeamMember> TeamMembers { get; set; }
+        public DbSet<TeamMember> TeamMembers { get; set; }
+        public DbSet<Project> Projects { get; set; }
+        public DbSet<FinalProjectContProg.Models.Task> Tasks { get; set; }
+        public DbSet<Skill> Skills { get; set; }
+    }
 }
