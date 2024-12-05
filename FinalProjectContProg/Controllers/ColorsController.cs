02d1983154f230b2a8de1263f29615f5edb509bf
Brian <faughtbn@mail.uc.edu>
2024-12-05 16:31:01 -0500

    Refactor ColorsController to improve query handling and response structure

--- FinalProjectContProg/Controllers/ColorsController.cs
+++ FinalProjectContProg/Controllers/ColorsController.cs
@@ -1,4 +1,3 @@
-
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -7,76 +6,79 @@ using Microsoft.EntityFrameworkCore;
 using FinalProjectContProg.Data;
 using FinalProjectContProg.Models;
 
-
-namespace FinalProjectContProg.Controllers{
+namespace FinalProjectContProg.Controllers
+{
     [Route("api/[controller]")]
     [ApiController]
+    public class ColorsController : ControllerBase
+    {
+        private readonly AppDbContext _context;
 
-    public class ColorsController : ControllerBase {
-        private readonly AppDbContext_context;
         public ColorsController(AppDbContext context)
         {
             _context = context;
         }
 
         [HttpGet]
-        public async Task<ActionResult<IEnumerable<Color>>>GetColors()
+        public async Task<ActionResult<IEnumerable<Color>>> GetColors([FromQuery] int? id)
         {
-            return await _context.Colors.ToListAsync();
+            if (id == null || id == 0)
+            {
+                return await _context.Colors.Take(5).ToListAsync();
+            }
+            
+            var color = await _context.Colors.Where(c => c.Id == id).ToListAsync();
+            return color.Count == 0 ? NotFound() : Ok(color);
         }
 
-        [HttpGet("{id}")]
-        public async Task<ActionResult<Color>> GetColor(int id){
+        [HttpPost]
+        public async Task<ActionResult<Color>> PostColor(Color color)
+        {
+            _context.Colors.Add(color);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction(nameof(GetColors), new { id = color.Id }, color);
+        }
+
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutColor(int id, Color color)
+        {
+            if (id != color.Id)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(color).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!_context.Colors.Any(c => c.Id == id))
+                {
+                    return NotFound();
+                }
+
+                throw;
+            }
+
+            return NoContent();
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteColor(int id)
+        {
             var color = await _context.Colors.FindAsync(id);
 
             if (color == null)
             {
                 return NotFound();
             }
-            return color;
 
-        }
-
-        [HttpPost]
-        public async Task<ActionResult<Color>> PostColor(Color color){
-            _context.Colors.Add(color);
-            await_context.SaveChangesAsync();
-
-            return CreatedAtAction("GetColor", new {id = color.ID}, color);
-
-        }
-
-        [HttpPut("{id}")]
-        public async Task<IActionResult> PutColor(int id, Color color){
-            if (id != color.Id){
-                return BadRequest();
-            }
-            _context.Entry(color).State=EntityState.Modified;
-
-            try{
-                await_context.SaveChangesAsync();
-            }
-            catch (DbUpdateconcurrencyException){
-                if (!_context.Colors.Any(this=>t.Id==id)){
-                    return NotFound();
-                }
-                else{
-                    throw;
-                }
-            }
-
-            return NotContext();
-        }
-
-        [HttpDelete("{id}")]
-        public async Task<IActionResult> DeleteColors(int id){
-            var color= await_context.Colors.FindAsync(id);
-
-            if (color==null){
-                return NotFound();
-            }
             _context.Colors.Remove(color);
-            await_context.SaveChangesAsync();
+            await _context.SaveChangesAsync();
 
             return NoContent();
         }
